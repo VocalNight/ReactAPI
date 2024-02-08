10365dba35713ad19357e6f88a2672af679e901c
Vocal <vocalnight@protonmail.com>
2024-02-08 15:55:28 -0300

    Created backbones of the API.

---
+++ ReactAPI/Model/APIContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReactAPI.Model
+{
+    public class APIContext: DbContext
+    {
+        public APIContext( DbContextOptions<APIContext> options ) : base(options) { }
+
+
+        protected override void OnModelCreating( ModelBuilder modelBuilder )
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<GameModel>().HasData(
+                    new GameModel { Id = 1, Name = "Tomb Raider", ReleaseDate = "1996", ImageUrl = "https://files.tecnoblog.net/wp-content/uploads/2021/09/tomb-raider-1.png" },
+                    new GameModel { Id = 2, Name = "Tomb Raider", ReleaseDate = "1996", ImageUrl = "https://files.tecnoblog.net/wp-content/uploads/2021/09/tomb-raider-1.png" },
+                    new GameModel { Id = 3, Name = "Tomb Raider", ReleaseDate = "1996", ImageUrl = "https://files.tecnoblog.net/wp-content/uploads/2021/09/tomb-raider-1.png" },
+                    new GameModel { Id = 4, Name = "Tomb Raider", ReleaseDate = "1996", ImageUrl = "https://files.tecnoblog.net/wp-content/uploads/2021/09/tomb-raider-1.png" }
+
+                );
+
+            }
+
+        public DbSet<GameModel> Games { get; set; }
+    }
+}
--- ReactAPI/Program.cs
+++ ReactAPI/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ReactAPI.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<APIContext>(opt =>
+opt.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,6 +19,14 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var db = scope.ServiceProvider.GetRequiredService<APIContext>();
+    db.Database.EnsureDeleted();
+    db.Database.EnsureCreated();
 }
 
 app.UseHttpsRedirection();
